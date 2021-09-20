a1653bcac09424551e4d7caebfa4f415cb0b05a1
Dorjee Tenzin <tedor18@gmail.com>
2021-09-20 17:32:13 +0000

    minor edits
    psql_docker.sh: removed exit at the end
    ddl:sql: removed unnecessary row

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -11,13 +11,12 @@ CREATE TABLE IF NOT EXISTS PUBLIC.host_info (
     cpu_mhz          FLOAT NOT NULL,
     L2_cache         SMALLINT NOT NULL,
     total_mem        INT NOT NULL,
-    "timestamp"        TIMESTAMP NOT NULL,
+    "timestamp"      TIMESTAMP NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS PUBLIC.host_usage (
-    id             SERIAL NOT NULL PRIMARY KEY,
     "timestamp"    TIMESTAMP NOT NULL,
-    host_id        SERIAL NOT NULL REFERENCES host_info(id) ON DELETE CASCADE,
+    host_id        INT NOT NULL REFERENCES host_info(id) ON DELETE CASCADE,
     memory_free    INT NOT NULL,
     cpu_idle       SMALLINT NOT NULL,
     cpu_kernel     SMALLINT NOT NULL,
